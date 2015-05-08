b4f604163513ca850c00ad12b6434756f8978340
Alex Dalton <alexmdalton@gmail.com>
2015-05-08 06:14:03 -0500

    adding my makefiles

--- common/c/fSetArray.c
+++ common/c/fSetArray.c
@@ -11,12 +11,12 @@ F2D* fSetArray(int rows, int cols, float val)
     int i, j;
     F2D *out;
     out = fMallocHandle(rows, cols);
-    
+
     for(i=0; i<rows; i++) {
         for(j=0; j<cols; j++) {
             subsref(out,i,j) = val;
 		}
-   	} 
+   	}
     return out;
-    
+
 }
